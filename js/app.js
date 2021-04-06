a4017bd7d718f7d001210633b00d8a098a551bcd
Christopher Buhendwa <68033904+karon16@users.noreply.github.com>
2021-04-06 12:36:48 +0100

    final html & css

--- js/app.js
+++ js/app.js
@@ -1,10 +1,43 @@
-$('.ui.dropdown')
-   .dropdown();
-   
 const navMenu = document.getElementById('navigation');
-
 window.onscroll = function(){
     navMenu.classList.add('menu--shadow');
-    console.log(navMenu.classList);
 }
+
+
+
+const portfolioCard = document.getElementById('hidden-card');
+const portfolioBtn = document.getElementById('portfolio-btn');
+const btnChange = document.querySelector('.btn--changeable')
+
+portfolioBtn.addEventListener('click', (e) =>{
+    e.preventDefault();
+    portfolioCard.classList.toggle('hidden-card');
+    btnChange.classList.add('change');
+    btnChange.innerHTML = "VOIR MOINS";
+    console.log("b")
+})
+
+// if(btnChange.classList.contains('change')){
+//     portfolioBtn.addEventListener('ciicked', () =>{
+//         e.preventDefault();
+//         portfolioCard.classList.add('hidden-card')
+//         btnChange.classList.remove('change');
+//         btnChange.innerHTML = "VOIR PLUS";
+//         console.log('a')
+//     })       
+// }
+
+const navSlide = function(){
+    const burger = document.querySelector('.burger');
+    const navigationLinkBox = document.querySelector('.navigation__link-box');
+    const navLinks = document.querySelectorAll('.navigation__link-box .nav-link');
+    const mainPage = document.querySelector('.main-page .header-background');
+
+    burger.addEventListener('click', () =>{
+        navigationLinkBox.classList.toggle('nav-active');
+        mainPage.classList.toggle('main-page--blacked')
+    })
+}
+
+navSlide();
 
