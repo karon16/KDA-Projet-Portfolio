485a50c3102debdf0890eb2d5d14f85ef9e72c5c
Christopher Buhendwa <68033904+karon16@users.noreply.github.com>
2021-04-08 01:54:14 +0100

    adding json

---
+++ js/ajax_about.js
@@ -0,0 +1,67 @@
+const blockIdentities = document.querySelector('#blocIdentities');
+
+fetch("http://localhost:3000/identities")
+.then(
+    function(response) {
+        return response.json();
+    }
+)
+.then(function (identities) {
+    for (const identity of identities) {
+
+        // creating and assigning element
+        const identitiesMainContainer = document.createElement('div');
+        identitiesMainContainer.classList.add("ui", "relaxed", "fluid", "stackable", "grid");
+
+        const profilePhotoContainer = document.createElement('div');
+        profilePhotoContainer.classList.add("five", "wide", "column", "about__profile");
+
+        const profilePhoto = document.createElement('img');
+        profilePhoto.classList.add("about__photo");
+        profilePhoto.setAttribute('alt', `${identity.alt}`);
+        profilePhoto.setAttribute('src', `${identity.image}`);
+
+        const biographyMainContainer = document.createElement('div');
+        biographyMainContainer.classList.add("eleven", "wide", "column", "middle", "aligned", "about__biography");
+
+        const biographyNames = document.createElement('span');
+        biographyNames.classList.add("bold-text--color");
+        biographyNames.textContent = `${identity.firstName} ${identity.lastName}`;
+
+        const biographyHeader = document.createElement('p');
+        biographyHeader.classList.add("paragraphs--font-size", "biography__greetings-size");
+        biographyHeader.textContent = `Hey, Je suis `;
+
+        const biographyParagraph = document.createElement('p');
+        biographyParagraph.classList.add("paragraphs--font-size", "biography__paragraph--text","biography__paragraph--margin", "paragraphs--font-family");
+        biographyParagraph.textContent = `${identity.Biography}`;
+
+        const cvButtonContainer = document.createElement('div');
+        cvButtonContainer.classList.add("sixteen", "wide", "column", "right", "aligned");
+
+        const cvButtonLink = document.createElement('a');
+        cvButtonLink.classList.add("btn", "btn--hidden", "btn--color");
+        cvButtonLink.setAttribute('target', '_blank');
+        cvButtonLink.setAttribute('href', `${identity.cvLink}`)
+        cvButtonLink.textContent = 'VOIR MON CV'
+
+        // adding element to DOM
+        // profile photo
+        profilePhotoContainer.appendChild(profilePhoto);
+        identitiesMainContainer.appendChild(profilePhotoContainer);
+
+        // paragraphs
+        biographyHeader.appendChild(biographyNames);
+        biographyMainContainer.appendChild(biographyHeader);
+        biographyMainContainer.appendChild(biographyParagraph);
+
+        // button
+        cvButtonContainer.appendChild(cvButtonLink);
+        biographyMainContainer.appendChild(cvButtonContainer);
+
+        // main Container
+        identitiesMainContainer.appendChild(biographyMainContainer);
+        blockIdentities.appendChild(identitiesMainContainer);
+
+    }
+});
