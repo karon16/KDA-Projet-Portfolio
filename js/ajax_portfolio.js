2dc7ff0343c0bfda9ee07476d80d4e0e6644a299
Christopher Buhendwa <christopherbuhendwa2000@gmail.com>
2021-04-20 20:04:06 +0100

    refactoring

--- js/ajax_portfolio.js
+++ js/ajax_portfolio.js
@@ -1,114 +1,82 @@
-const portfolioRow1 = document.querySelector('#portfolioRow1');
-const portfolioRow2 = document.querySelector('#hidden-card')
-fetch('https://my-json-server.typicode.com/karon16/KDA-Projet-Portfolio/portfolios')
-.then(
-    function(response) {
-        return response.json();
-    }
-).then(function (portfolios) {
-    for (const row of portfolios) {
-        let portfolio1;
+const portfolioRows = [
+  document.querySelector("#portfolioRow1"),
+  document.querySelector("#hidden-card"),
+];
 
-        if (portfolio1 = row.portfolio1) {
-            for (const card of portfolio1) {
-                const portfolioCard = document.createElement('div');
-                portfolioCard.classList.add("column");
-        
-                const portfolioCardMainContainer = document.createElement('div');
-                portfolioCardMainContainer.classList.add("ui", "fluid", "card");
-        
-                const ImageBox = document.createElement('div');
-                ImageBox.classList.add("ui", "slide", "masked", "reveal", "image");
-        
-                const visibleImage = document.createElement('img');
-                visibleImage.classList.add("visible", "content");
-                visibleImage.setAttribute('src', `${card.image1}`);
-                visibleImage.setAttribute('alt', `${card.alt1}`);
-        
-                const hiddenImage = document.createElement('img');
-                hiddenImage.classList.add("hidden", "content");
-                hiddenImage.setAttribute('src', `${card.image2}`);
-                hiddenImage.setAttribute('alt', `${card.alt2}`);
-        
-                const textBox = document.createElement('div');
-                textBox.classList.add('content');
-        
-                const link = document.createElement('a');
-                link.classList.add('header');
-                link.setAttribute('href', `${card.url}`);
-                link.textContent = `${card.title}`
-        
-                const description = document.createElement('div');
-                description.classList.add('description');
-                description.textContent = `${card.description}`;
-        
-                ImageBox.appendChild(visibleImage);
-                ImageBox.appendChild(hiddenImage);
-        
-                portfolioCardMainContainer.appendChild(ImageBox);
-        
-                textBox.appendChild(link);
-                textBox.appendChild(description);
-        
-                portfolioCardMainContainer.appendChild(textBox);
-        
-                portfolioCard.appendChild(portfolioCardMainContainer);
-        
-                portfolioRow1.appendChild(portfolioCard);
-            }
-        }
+// function createHtmlElements() {}
 
-        let portfolio2;
-        if (portfolio2 = row.portfolio2) {
-            for (const card of portfolio2) {
-                const portfolioCard = document.createElement('div');
-                portfolioCard.classList.add("column");
-        
-                const portfolioCardMainContainer = document.createElement('div');
-                portfolioCardMainContainer.classList.add("ui", "fluid", "card");
-        
-                const ImageBox = document.createElement('div');
-                ImageBox.classList.add("ui", "slide", "masked", "reveal", "image");
-        
-                const visibleImage = document.createElement('img');
-                visibleImage.classList.add("visible", "content");
-                visibleImage.setAttribute('src', `${card.image1}`);
-                visibleImage.setAttribute('alt', `${card.alt1}`);
-        
-                const hiddenImage = document.createElement('img');
-                hiddenImage.classList.add("hidden", "content");
-                hiddenImage.setAttribute('src', `${card.image2}`);
-                hiddenImage.setAttribute('alt', `${card.alt2}`);
-        
-                const textBox = document.createElement('div');
-                textBox.classList.add('content');
-        
-                const link = document.createElement('a');
-                link.classList.add('header');
-                link.setAttribute('href', `${card.url}`);
-                link.textContent = `${card.title}`
-        
-                const description = document.createElement('div');
-                description.classList.add('description');
-                description.textContent = `${card.description}`;
-        
-                ImageBox.appendChild(visibleImage);
-                ImageBox.appendChild(hiddenImage);
-        
-                portfolioCardMainContainer.appendChild(ImageBox);
-        
-                textBox.appendChild(link);
-                textBox.appendChild(description);
-    
-                portfolioCardMainContainer.appendChild(textBox);
-                portfolioCard.appendChild(portfolioCardMainContainer);   
-                portfolioRow2.appendChild(portfolioCard);
-            }
-        }
-    }
+function renderCard(card) {
+  const portfolioCard = document.createElement("div");
+  portfolioCard.classList.add("column");
 
-});
+  const portfolioCardMainContainer = document.createElement("div");
+  portfolioCardMainContainer.classList.add("ui", "fluid", "card");
 
+  const ImageBox = document.createElement("div");
+  ImageBox.classList.add("ui", "slide", "masked", "reveal", "image");
 
+  const visibleImage = document.createElement("img");
+  visibleImage.classList.add("visible", "content");
+  visibleImage.setAttribute("src", `${card.image1}`);
+  visibleImage.setAttribute("alt", `${card.alt1}`);
 
-    
+  const hiddenImage = document.createElement("img");
+  hiddenImage.classList.add("hidden", "content");
+  hiddenImage.setAttribute("src", `${card.image2}`);
+  hiddenImage.setAttribute("alt", `${card.alt2}`);
+
+  const textBox = document.createElement("div");
+  textBox.classList.add("content");
+
+  const link = document.createElement("a");
+  link.classList.add("header");
+  link.setAttribute("href", `${card.url}`);
+  link.textContent = `${card.title}`;
+
+  const description = document.createElement("div");
+  description.classList.add("description");
+  description.textContent = `${card.description}`;
+
+  ImageBox.appendChild(visibleImage);
+  ImageBox.appendChild(hiddenImage);
+
+  portfolioCardMainContainer.appendChild(ImageBox);
+
+  textBox.appendChild(link);
+  textBox.appendChild(description);
+
+  portfolioCardMainContainer.appendChild(textBox);
+
+  portfolioCard.appendChild(portfolioCardMainContainer);
+
+  return portfolioCard;
+}
+
+function renderPortfolio(portfolio, portfolioRow) {
+  for (const card of portfolio) {
+    const portfolioCard = renderCard(card);
+    portfolioRow.appendChild(portfolioCard);
+    console.log(portfolio)
+
+  }
+}
+
+function getAndRenderPortfolios(portfolios) {
+  let index = 0;
+  for (const portfolio of portfolios) {
+    renderPortfolio(portfolio, portfolioRows[index]);
+    index++;
+  }
+}
+
+const fetchUrl =
+  "https://my-json-server.typicode.com/karon16/KDA-Projet-Portfolio/portfolios";
+
+fetch(fetchUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (portfolios) {
+    getAndRenderPortfolios(portfolios);
+  });
+
